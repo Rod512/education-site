1185b28e4e5b8ac4b577ba86d495185e637f90ad
Rod512 <rudro149187@bsdi-bd.org>
2022-07-04 04:42:30 +0600

    project on procesed

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window_scroll',window.scrollY > 0)
+})
+
+// =====faq=====//
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open')
+
+        let icon = faq.querySelector('.faq_icon i')
+
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus'
+        }else{
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
+});
+
+// =====faq=====//
+
+const menu = document.querySelector(".nav_menu")
+const menuBtn = document.querySelector("#open-menu-btn")
+const closeBtn = document.querySelector("#close-menu-btn")
+
+const opennav = () =>{
+    menu.style.display = 'flex'
+    closeBtn.style.display = "inline-block"
+    menuBtn.style.display = 'none'
+}
+
+const closenav = () => {
+    menu.style.display = 'none'
+    closeBtn.style.display = "none"
+    menuBtn.style.display = 'inline-block'
+}
+
+menuBtn.addEventListener('click', opennav)
+closeBtn.addEventListener('click',closenav)
